b6835cb03d76dfabb51c57f5d2cc881ba040e38a
Hayden <hudgy.1@gmail.com>
2018-05-28 16:58:44 +0200

    Clean Slate

---
+++ NMAttack.py
@@ -0,0 +1,46 @@
+import scapy.all as scapy
+import sys
+
+switch(sys.argv[1]){
+         case "ipsweep":;
+            break;
+         case "portscan":;
+            break;
+         case "ipspoof":;
+            break;
+         case "SFflag":;
+            break;
+         case "Fflag":;
+            break;
+         case "URGflag":;
+            break;
+         case "NOflag":;
+            break;
+         case "SYNflood":;
+            break;
+         case "ICMPflood":;
+            break;
+         case "DropConnection":;
+            break;
+         case "ICMPredirect":;
+            break;
+         case "UDPflood":;
+            break;
+         case "LandAttack":;
+            break;
+         case "TeardropAttack":;
+            break;
+         case "PingDeath":;
+            break;
+         case "ARPpoisoning":;
+            break;
+         case "MACflood":;
+            break;
+         case: "PortStealing":;
+            break;
+         case: "RIPpoisoning":;
+            break;
+         
+         default: print("Invalid your dumb");
+               break;
+      }
